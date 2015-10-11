46b83902c4d8255b56d779539d99a210c0971843
Joris Röling <joris@bonboa.com>
2015-10-11 12:25:31 +0200

    v

--- README.md
+++ README.md
@@ -11,4 +11,4 @@ meteor add jorisroling:orion-facts
 
 #### Security
 
-Orion Factses uses [```nicolaslopezj:roles```](http://github.com/nicolaslopezj/roles) to secure the import and facts. The name of the action is ```jorisroling.orionFacts```.
+Orion Facts uses [```nicolaslopezj:roles```](http://github.com/nicolaslopezj/roles) to secure the import and facts. The name of the action is ```jorisroling.orionFacts```.
--- package.js
+++ package.js
@@ -1,13 +1,13 @@
 Package.describe({
   name: 'jorisroling:orion-facts',
   summary: 'Meteor Facts in Orion',
-  version: '0.0.2',
+  version: '0.0.3',
   git: 'https://github.com/jorisroling/orion-facts'
 });
 
 Package.onUse(function(api) {
   api.versionsFrom('1.0');
-  api.use(['meteor-platform', 'orionjs:base@1.5.0','nicolaslopezj:roles@1.2.0', 'facts']);
+  api.use(['meteor-platform', 'orionjs:base@1.5.0','nicolaslopezj:roles@1.5.2', 'facts']);
 
   api.use(['orionjs:bootstrap@1.5.0'], 'client', { weak: true });
 
