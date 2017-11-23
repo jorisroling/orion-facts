7bc68b4c728e0462e56b54cdd7ba134577aec574
Joris Röling <j.roling@tmg.nl>
2017-11-23 10:17:00 +0100

    v0.0.5

--- package.js
+++ package.js
@@ -1,15 +1,15 @@
 Package.describe({
   name: 'jorisroling:orion-facts',
   summary: 'Meteor Facts in Orion',
-  version: '0.0.3',
+  version: '0.0.5',
   git: 'https://github.com/jorisroling/orion-facts'
 });
 
 Package.onUse(function(api) {
   api.versionsFrom('1.0');
-  api.use(['meteor-platform', 'orionjs:base@1.5.0','nicolaslopezj:roles@1.5.2', 'facts']);
+  api.use(['meteor-platform', 'orionjs:base@1.6.0','nicolaslopezj:roles@2.0.0', 'facts']);
 
-  api.use(['orionjs:bootstrap@1.5.0'], 'client', { weak: true });
+  api.use(['orionjs:bootstrap@1.6.0'], 'client', { weak: true });
 
   api.addFiles('facts.js');
   api.addFiles('facts_server.js', 'server');
